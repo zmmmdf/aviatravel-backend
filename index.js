e6c196c0c8203176efe883d75d4cc861787a5827
Ziya <151031540+zmmmdf@users.noreply.github.com>
2024-11-21 21:23:57 +0400

    update index /

--- index.js
+++ index.js
@@ -6,19 +6,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 
-const serviceAccount = {
-  "type": "service_account",
-  "project_id": "aviatravel-az",
-  "private_key_id": "1b15765cdd41bc85eab3050b7c9f07cae4dcb616",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDLf9LSzRW3mICi\nDcTf+1ecf+fxpncg2I1QTWY2jLYr0DQZezUIT6Rx3M0FUTL10iR0a3dAD1/ckJ2D\nqNWCjZfwCy0z0zVujJ6ouFaqxZBQU6aPSXjE6/XaPL245KTCEUOr9RufMwa9mWzH\nZPAqd2mKFG8Jw4NsQxDyDsRVudTJsZPnKqNE1KM4OxllWulfcpkMF1qruS5bwo+s\nO2JbJb6WAjsXgSaicjEzGp6SmGR6CQSoFcH8xvHP70N0tNUtugR5+7UoUGjswf5d\niD6g04a2fa0WoEtQzyqtPiQGCl9HpZP2vpL9r0NAurTEHdXdZtD80NeZr+GhdoM1\nXBVx6h1vAgMBAAECggEAMy4ONUMsE5UwwNEKszRQ6ATJLl7p9iJQhCI7Mzv3cxR1\njfUNNO5m0tYv4Ab4dJep4HfIsZ1MRDpAy0SSkjePP2loMHENrqSCILff4Co47eLj\n8ISPcaNQnVcKdi5W+5IA/YXzMPpR1yx3jbxmpJ+MtRQHiyKXcop5gA9TtKW8jQ7/\nvIfLde3T6H3VmU/qo+Fh2gHajnOexrHA+u/VJUkYDNFvcZ9z/z+kDRLUjZefO9J+\nn6pU0ZNKfRxUTOx6KDtWUIz2nfjxvfZ8D4tPRM1NMDMPM8Nt/8bu5mtaugMlfaww\nUVETuGgeI9IBrgAD+ecVvj7PfoP/wOvX5WxpY8fvDQKBgQDyC37GDU3w+24+hY6g\ncPU8EihwkvQadijPZf6TzONRN5lSDjBN3uZWZL6FoL/5yp4Ki5faiAsEgxk306dw\naVGrw1vM4AQuLouKd3aIz7OfcWoiRYTP/Jd9DPp7KFhuyCxKGmf+1/OHVzWWKnPi\nGt7SdlYk0/yhaotSj80i4vCdbQKBgQDXO2hlF4LQ3GRDfDV/McmykKYh27pRi3Zc\nC7uZFhRu0IlSG193yAByKoyvqDMP32g0l9g8fjrfqjcOrVlbh8mqvtIuPOOLAQ6r\nCRhSY7QHVbjuqiTrwF2As/5HEL/fCw+8WhtvBu3KrwiMUsRJUysk2C68uRSzKFVA\nJKQgtS1oywKBgQCbr30pkuExQehVWr60wDNdIijU0BEm/B2f2XD4TG9ijCqYNvwm\n+lqZ808A0LbqysMA8W0JaTv8+P49hGxMx9F6c7wWeXj244Vp2RFrSn207tRfmuuk\nhEpEaxaqJPdMuoCSYY3Gw8ymnxeesUu8gaAVK3yZT6lNgiR/RHOkHui5uQKBgQCV\nlREhqOxekb0Q9cMMSiLeeg8r2tSrlZxFrlXLt/+tYDr/mEyKJ7K/pXivH9uU/XQj\nkSBUMnpiNZ8cETfYGA/9Ha9fOSv+0FZMYf7IS8izjE0Bebg9LC0PpIhlBWwD7GEc\nSxdj21HqGil7tI38U9PZQHGC8bJELRTKhmyW2rPbbwKBgBWDChxmivZnDAWOzNO3\naI2jrrjnRrKyP1+OSjoKuXVCdIWXFecysNxwHnH1ESBXXV31d3AXv4Tu5rK8bxTb\ndnXf5L4RPA0rrz3D9NiV1mMczBLRrKIn0FL0oJbf+Dv38hWsJIhbd4ZJ9i2zw1/a\n6VD66rU+UIYwt5i6vjB7BCpE\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-4esmh@aviatravel-az.iam.gserviceaccount.com",
-  "client_id": "111616933634326200002",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-4esmh%40aviatravel-az.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-};
+const serviceAccount = require('./serviceAccountKey.json'); // Replace with the path to your Firebase Admin SDK key file
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -41,7 +30,10 @@ const upload = multer({ storage });
 
 
 app.get('/', (req, res) => {
-    res.json({ "Message": "Server is running" });
+    res.json({ 
+      "Firebase": serviceAccount,
+      "Message": "Server is running" 
+    });
 });
 
 
